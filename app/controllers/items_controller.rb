7876a0a4813c66bc0f687a6fac9dd4ab636c31b7
Nicki Nixon <nickitza@gmail.com>
2019-05-15 16:35:24 -0600

    build view pages for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,58 @@
 class ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = @department.items
   end
 
   def show
   end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to department_item_path(@department.id, @item.id)
+    else
+      render partial: "form"
+    end
+  end
+
+  def new
+    @item = @department.items.new
+    render partial: "form"
+  end
+  
+  def create
+    @item = @department.items.new(item_params)
+    if @item.save
+      redirect_to department_item_path(@department.id, @item.id)
+    else 
+      render partial: "form"
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path(@department.id)
+  end
+
+# ============= PRIVATE METHODS ========================
+  private
+  def set_department
+    @department = Department.find(params[:department_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :in_stock)
+  end
+
+
 end
