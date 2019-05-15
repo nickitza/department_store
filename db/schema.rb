504da2bb59c18f3ebb1030000887a8bde172cb2a
Nicki Nixon <nickitza@gmail.com>
2019-05-15 13:49:02 -0600

    add Departments controller

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2019_05_15_194620) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
